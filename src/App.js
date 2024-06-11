2cb7237602c836080574f837aa91cc3711a6f7bb
lgy <ini03134@naver.com>
2024-06-11 15:01:00 +0900

    Modify: "헤더 module.scss 파일명 변경"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Reset } from 'styled-reset';
 import './App.css';
 import NewsList from './components/board/NewsList';
+import Header from './common/layout/Header';
+import Footer from './common/layout/Footer';
 
 function App() {
   return (
     <div className='content-wrapper'>
       <Reset />
+      <Header />
       <NewsList />
+      <Footer />
     </div>
   );
 }
--- src/common/layout/Footer.js
+++ src/common/layout/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from '../../styles/Footer.module.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <img
+          src={require('../../assets/img/logo.png')}
+          alt='로고이미지'
+          height='90'
+        />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
