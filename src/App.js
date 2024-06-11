b9f16a23518724bd0b28aa4524aaa333fc18f443
lgy <ini03134@naver.com>
2024-06-11 18:08:43 +0900

    Feat: "메인페이지 구성중(워드클라우드 페이지 구현 진행)"

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import NewsList from './components/board/NewsList';
 import Header from './common/layout/Header';
 import Footer from './common/layout/Footer';
+import Intro from './components/main/Intro';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from './components/main/Home';
 
 function App() {
   return (
-    <div className='content-wrapper'>
-      <Reset />
-      <Header />
-      <NewsList />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Intro />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/newsList' element={<NewsList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
